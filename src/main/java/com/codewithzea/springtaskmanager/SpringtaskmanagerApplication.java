6325de99f2d11d97167398db303c883fcc5a7c52
YawFrimpong139 <yawzealow@gmail.com>
2025-05-28 01:58:01 +0100

    chore: Initial project setup

---
+++ src/main/java/com/codewithzea/springtaskmanager/SpringtaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.codewithzea.springtaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtaskmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtaskmanagerApplication.class, args);
+    }
+
+}
